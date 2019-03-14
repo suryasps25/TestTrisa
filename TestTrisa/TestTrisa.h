7320ad976b2dd5b7911bf7362603aa890c6aba41
Surya SaravanaKumar <surya@agiliztech.com>
2019-03-14 15:48:41 +0530

    Initial Commit

---
+++ TestTrisa/TestTrisa.h
@@ -0,0 +1,19 @@
+//
+//  TestTrisa.h
+//  TestTrisa
+//
+//  Created by Surya on 14/03/19.
+//  Copyright © 2019 agiliztech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestTrisa.
+FOUNDATION_EXPORT double TestTrisaVersionNumber;
+
+//! Project version string for TestTrisa.
+FOUNDATION_EXPORT const unsigned char TestTrisaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestTrisa/PublicHeader.h>
+
+
